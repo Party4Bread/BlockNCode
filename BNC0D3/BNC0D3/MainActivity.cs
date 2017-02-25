4e207e0124fbe403cc8ffdd0b690079919c83d7f
inbread <solsam10@naver.com>
2017-02-26 00:22:47 +0900

    SET Client Base

---
+++ BNC0D3/BNC0D3/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace BNC0D3
+{
+    [Activity(Label = "BNC0D3", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
